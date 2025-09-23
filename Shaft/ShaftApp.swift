1f6bdc8450460594d8c46a7876a17954caf3a35c
SoxiaLiSA <ceuilisa@outlook.com>
2025-09-23 15:16:33 +0800

    Initial Commit

---
+++ Shaft/ShaftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaftApp.swift
+//  Shaft
+//
+//  Created by 杨鑫 on 2025/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ShaftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
